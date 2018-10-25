de954148efd36a2724e7b67180334a234dcbb572
Mae Bautch <mbautch@outlook.com>
2018-10-25 13:48:04 -0500

    Can I commit?

---
+++ ExpertInvoAPI/Controllers/PlcController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpertInvoAPI.Data;
+using ExpertInvoAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpertInvoAPI.Controllers
+{
+	[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlcController: ControllerBase
+    {
+        ApplicationDbContext _Context;
+        public PlcController(ApplicationDbContext databasecontext)
+        {
+            _Context = databasecontext; //abstracts dbcontext into _context
+        }
+
+        public List<PlcTb> plcTbsList { get; set; }
+        public void OnGet()
+        {
+            var data = (from plcTbsList in _Context.PlcKey
+                        select plcTbsList).ToList(); //specifies what to grab
+
+            plcTbsList = data; //grabs data
+        }
+
+        [HttpPost]
+        public String Indexhome(IEnumerable<PlcTb> Entry)
+        {
+            if (Entry == null)
+            {
+                return "No entry selected";
+            }
+            else
+            {
+                return "You selected " + string.Join(", ", Entry.Select(p => p.Register));
+            }
+        }
+
+        [BindProperty]
+        public PlcTb Entry { get; set; }
+
+        //to delete
+        public ActionResult OnGetDelete(int id)
+        {
+            if (id != null) //always evaluates to true in warnings??
+            {
+                var data = (from Entry in _Context.PlcKey
+                            where Entry.Id ==id
+                            select Entry).SingleOrDefault();
+                _Context.Remove(data); //deletes from db
+                _Context.SaveChanges(); //saves changes to db
+            }
+            return RedirectToPage("insertpagehere"); //no page to redirect to at the moment
+        }
+
+        public ActionResult OnPost()
+        {
+            var entry = Entry;
+            if(!ModelState.IsValid) //checks model state
+            {
+                return RedirectToPage("insertpagehere");  //no page to redirect to at the moment
+            }
+            entry.Id = 0;
+            var result = _Context.Add(entry);
+            _Context.SaveChanges(); //Saves entries
+
+            return RedirectToPage("insertpagehere"); //no page to redirect to at the moment
+        }
+    }
+}
---
+++ ExpertInvoAPI/Models/EmployeesTb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpertInvoAPI.Models
+{
+    [Table("tblEmployees")]
+    public class EmployeesTb
+    {
+        public int ID { get; set; }
+        [Required (ErrorMessage = "First Name is not found!!")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is not found!!")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Address is not found!!")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Age is not found!!")]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Birth Date is not found!!")]
+        public DateTime Birthdate { get; set; }
+        [Required(ErrorMessage = "Personal Email is not found!!")]
+        public string PersonalEmail { get; set; }
+        [Required(ErrorMessage = "Primary Phone is not found!!")]
+        public int PrimaryPhone { get; set; }
+        [Required(ErrorMessage = "Secondary Phone is not found!!")]
+        public int SecondaryPhone { get; set; }
+        [Required(ErrorMessage = "Emergency Contact is not found!!")]
+        public string EmergencyContact { get; set; }
+        [Required(ErrorMessage = "Emergency Contact Number is not found!!")]
+        public int EmergencyContactNumber { get; set; }
+    }
+}
