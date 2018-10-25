1a1427015ad6e217741ba218a76ad5f2446c4838
Mae Bautch <mbautch@outlook.com>
2018-10-25 14:24:22 -0500

    Trying to sync

--- ExpertInvoAPI/Models/EmployeesTb.cs
+++ ExpertInvoAPI/Models/EmployeesTb.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+//Since this page is for a table, this page is probs not necessary for just an API
 namespace ExpertInvoAPI.Models
 {
     [Table("tblEmployees")]
